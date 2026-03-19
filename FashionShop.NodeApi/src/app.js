b759c67c3d110e5a3506ef90f24f8a92f0b4b812
Mèo Ú <nguyennhathung17@gmail.com>
2026-03-19 20:09:18 +0700

    thêm Cloudflare

--- FashionShop.NodeApi/src/app.js
+++ FashionShop.NodeApi/src/app.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://athea.vn', 'https://www.athea.vn', 'http://localhost:3000', 'http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
