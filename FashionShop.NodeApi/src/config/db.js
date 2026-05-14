bc66307e97bf2698b087a33a4db08a31df4bf722
Mèo Ú <nguyennhathung17@gmail.com>
2026-05-14 18:04:43 +0700

    fix Can't reach database server

--- FashionShop.NodeApi/src/config/db.js
+++ FashionShop.NodeApi/src/config/db.js
@@ -1,10 +1,24 @@
 const { PrismaClient } = require('@prisma/client');
+const { Pool, neonConfig } = require('@neondatabase/serverless');
+const { PrismaNeon } = require('@prisma/adapter-neon');
+const ws = require('ws');
+
 require('dotenv').config();
+
+// Fix cho môi trường Node.js không có sẵn WebSocket (cần cho serverless)
+neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = global;
 
 if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient();
+    const connectionString = process.env.DATABASE_URL;
+    
+    // Khởi tạo Pool serverless của Neon
+    const pool = new Pool({ connectionString });
+    
+    // Gắn Adapter Neon vào Prisma
+    const adapter = new PrismaNeon(pool);
+    globalForPrisma.prisma = new PrismaClient({ adapter });
 }
 
 module.exports = globalForPrisma.prisma;
