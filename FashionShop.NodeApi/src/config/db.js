5f6ebcf8e59652d9b17ef7aa1434b3daec7bee46
Mèo Ú <nguyennhathung17@gmail.com>
2026-05-14 18:06:52 +0700

    back lại

--- FashionShop.NodeApi/src/config/db.js
+++ FashionShop.NodeApi/src/config/db.js
@@ -1,24 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
-const { Pool, neonConfig } = require('@neondatabase/serverless');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-const ws = require('ws');
-
 require('dotenv').config();
-
-// Fix cho môi trường Node.js không có sẵn WebSocket (cần cho serverless)
-neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = global;
 
 if (!globalForPrisma.prisma) {
-    const connectionString = process.env.DATABASE_URL;
-    
-    // Khởi tạo Pool serverless của Neon
-    const pool = new Pool({ connectionString });
-    
-    // Gắn Adapter Neon vào Prisma
-    const adapter = new PrismaNeon(pool);
-    globalForPrisma.prisma = new PrismaClient({ adapter });
+    globalForPrisma.prisma = new PrismaClient();
 }
 
 module.exports = globalForPrisma.prisma;
